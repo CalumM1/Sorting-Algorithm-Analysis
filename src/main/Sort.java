dc467c7304a3fb75553b23443f1484e99e766a22
Calum McCarthy <2004mccarthy@gmail.com>
2025-02-20 14:26:21 +0000

    create swap and find min helpers

--- src/main/Sort.java
+++ src/main/Sort.java
@@ -30,23 +30,28 @@ public class Sort
             int sorted = array[i];
 
             int index = i;
-            int min = sorted;
-            for (int j = i + 1; j < array.length; j++)
-            {
-                int next = array[j];
-                if (next < min)
-                {
-                    min = next;
-                    index = j;
-                }
-            }
+            index = findMin(array, i, sorted, index);
             swap(array, i, index);
         }
         return array;
     }
 
 
-    public static void swap(int[] array, int index1, int index2)
+    private static int findMin(int[] array, int i, int min, int index) {
+        for (int j = i + 1; j < array.length; j++)
+        {
+            int next = array[j];
+            if (next < min)
+            {
+                min = next;
+                index = j;
+            }
+        }
+        return index;
+    }
+
+
+    private static void swap(int[] array, int index1, int index2)
     {
         int temp = array[index1];
         array[index1] = array[index2];
